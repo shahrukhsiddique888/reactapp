ea91ccb6d8824e6c7dca04fe3d3a372cb6f841b4
sidd <shahrukhsiddiqui9819@gmail.com>
2023-05-16 01:55:19 +1000

    hello there

--- src/App.js
+++ src/App.js
@@ -1,24 +1,155 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Srk from './Srk';
+import Body from './Body';
+import Foot from './foot';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+         <div>
+             <Srk />
+             <Body />
+             <Foot />
+           </div>
+          //  <Navbar className='bg-warning' expand='md'>
+          //   <Container >
+           
+          //     <NavbarBrand  className='d-sm-flex'>
+          //       <img  src={ logo } width='30px' height='30px' alt='h'/>
+          //         hell
+          //     </NavbarBrand>
+            
+          //     <NavbarToggle aria-controls='basic-navbar-nav'></NavbarToggle>
+          //     <NavbarCollapse id='basic-navbar-nav'>
+          //       <Nav className='me-auto' >
+               
+          //           <NavLink>hello</NavLink>
+          //           <NavLink>hell</NavLink>
+                  
+          //       </Nav>
+
+          //     </NavbarCollapse>
+
+
+            
+          //   </Container>
+
+
+
+          //  </Navbar>
+
+           
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // <Navbar bg='warning' expand='md'>
+        //   <img src={logo} height={25} alt='he'/>
+        //   <NavbarBrand href='#h'>fuck all ya</NavbarBrand>
+        //   <NavbarToggle aria-controls='baisc-navbar-nav'></NavbarToggle>
+        //   <NavbarCollapse id='basic-navbar-nav'>
+        //    <Nav className='me-auto'>
+        //     <NavLink >hel</NavLink>
+        //     <NavLink>hello</NavLink>
+        //     <NavDropdown title='shahrukh'>
+        //       <NavDropdown.Item href='#l'>hello</NavDropdown.Item>
+        //     </NavDropdown>
+
+        //    </Nav>
+
+
+        //   </NavbarCollapse>
+
+
+          
+
+
+        // </Navbar>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // <Navbar bg='warning' expand='md'>
+      //   <NavbarBrand href='#home'> navbar</NavbarBrand>
+      //   <NavbarToggle aria-controls='basic-navbar-nav'></NavbarToggle>
+      //   <NavbarCollapse id='basic-navbar-nav'>
+      //     <Nav className='me-auto'>
+      //      <NavLink href='#h'>hr</NavLink>
+      //      <NavLink href='#h'>dir</NavLink>
+      //      <NavLink href='#h'>he</NavLink>
+      //      <NavDropdown title='shahrukh' id='basic-nav-dropdown'>
+      //         <NavDropdown.Item href='#hel'>  the</NavDropdown.Item>
+      //         <NavDropdown.Divider /> 
+      //         <NavDropdown.Item href='#hel'>  the</NavDropdown.Item>
+      //         <NavDropdown.Item href='#hel'>  the</NavDropdown.Item>
+      //      </NavDropdown>
+      //     </Nav>
+      //   </NavbarCollapse>
+      // </Navbar>
+
+      
+      //   <Navbar bg="warning" expand="md">  
+      //   <Container>  
+      //     <Navbar.Brand href="#home">Navbar Brand</Navbar.Brand>  
+      //     <Navbar.Toggle aria-controls="basic-navbar-nav" />  
+      //     <Navbar.Collapse id="basic-navbar-nav">  
+      //       <Nav className="me-auto">  
+      //         <Nav.Link href="#home">Home</Nav.Link>  
+      //         <Nav.Link href="#link">Link</Nav.Link>  
+      //         <NavDropdown title="Dropdown" id="basic-nav-dropdown">  
+      //           <NavDropdown.Item href="#action/3.1">Dropdown Item 1</NavDropdown.Item>  
+      //           <NavDropdown.Item href="#action/3.2">Dropdown Item 2</NavDropdown.Item>  
+      //           <NavDropdown.Item href="#action/3.3">Dropdown Item 3</NavDropdown.Item>  
+      //           <NavDropdown.Divider />  
+      //           <NavDropdown.Item href="#action/3.4">Another Item</NavDropdown.Item>  
+      //         </NavDropdown>  
+      //       </Nav>  
+      //     </Navbar.Collapse>  
+      //   </Container>  
+      // </Navbar> 
+      // <div className='App'>
+      //   <Navbar className='nav navbar-expand-lg bg-dark navbar-dark'>
+      //     <a href='#' className='navbar-brand'>front </a>
+      //   </Navbar>
+      // </div>       
+      // <div className="App m-0 d-xl-flex vh-100 bg-warning">
+      //   <div className='container m-0 bottom-full bg-secondary d-lg-flex p-2 justify-start'>
+      //     <Button type="button" className="btn btn-warning vh-none" data-bs-toggle="button">Toggle button</Button>
+      //     <Button type="button" className="btn btn-danger active" data-bs-toggle="button" aria-pressed="true">Active toggle button</Button>
+      //     <Button type="button" className="btn btn-secondary" disabled data-bs-toggle="button">Disabled toggle button</Button>
+      //     <Button type="button" className="btn btn-warning" data-bs-toggle="button">Toggle button</Button>
+      //     <Button type="button" className="btn btn-danger active" data-bs-toggle="button" aria-pressed="true">Active toggle button</Button>
+      //     <Button type="button" className="btn btn-secondary" disabled data-bs-toggle="button">Disabled toggle button</Button>
+      //   </div>
+      // </div>
   );
 }
 
